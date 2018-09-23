1fef33863c5022946751749881ea039ca27a0e96
Mac <gizemkorkmaz219@gmail.com>
2018-09-23 15:56:24 +0300

    sil ve ekle butonu eklendi

--- src/pages/FlatListExmPage.js
+++ src/pages/FlatListExmPage.js
@@ -2,33 +2,28 @@ import React, { Component } from 'react';
 import { View, Text ,StyleSheet,FlatList} from 'react-native';
 import { DummyData } from '../components/flatListExmPage/DummyData';
 import {Button} from "react-native-paper";
-// data=[
-//     {
-//         firstName:"Ayşe",
-//         lastName:"Kaya",
-//         phone:"05556376378"
+data=[
+    {
+        firstName:"Ayşe",
+        lastName:"Kaya",
+        phone:"05556376378"
 
 
-//     },
-//     {
-//     firstName:"Gaye",
-//     lastName:"Akbaba",
-//     phone:"05559206378"
+    },
+    {
+    firstName:"Gaye",
+    lastName:"Akbaba",
+    phone:"05559206378"
 
 
-// },
-// {
-//     firstName:"Mehmet",
-//     lastName:"Açar",
-//     phone:"05413568790"
-// },
-// {
-//     firstName:"Saygı",
-//     lastName:"Değer",
-//     phone:"05356784352"
-// }
+},
+{
+    firstName:"Mehmet",
+    lastName:"Açar",
+    phone:"05413568790"
+}
     
-// ];
+];
 
 
 class FlatListExmPage extends Component{
@@ -36,16 +31,16 @@ state={
     isShowListVisible:false,
     data:[]
 };
-componentDidMount(){
-    this.fetchData();
-}
+// componentDidMount(){
+//     this.fetchData();
+// }
 
-fetchData=async ()=>{
-    const response= await fetch("http://www.json-generator.com/api/json/get/bUnhveDRVe?indent=2");
-    const json= await response.json();
-    this.setState({data:json.people});
+// fetchData=async ()=>{
+//     const response= await fetch("http://www.json-generator.com/api/json/get/bUnhveDRVe?indent=2");
+//     const json= await response.json();
+//     this.setState({data:json.indent});
     
-};
+// };
 
     renderFlatList(){
         if(this.state.isShowListVisible===true){
@@ -53,16 +48,23 @@ fetchData=async ()=>{
                 <View style={styles.list} >
 
                 <FlatList   
-                data={this.state.data}
+                // data={this.state.data}
+                data={data}
                 keyExtractor={(item,index)=>index.toString()}
                 renderItem={({item,index})=>{
                     return (
-                        <View key={index} style={styles.container} >
-                
+                        <View key={index}  >
+{/*                 
                         <Text   >{item.people.phone}</Text>
                         <Text    >{item.people.age}</Text>
-                        <Text   >{item.people.name}</Text>
+                        <Text   >{item.people.name}</Text> */}
+                           <Text   >{item.firstName}</Text>
+                        <Text    >{item.lastName}</Text>
+                        <Text   >{item.phone}</Text> 
+                        
                         </View>
+
+
                 
                 
                     )
@@ -72,6 +74,10 @@ fetchData=async ()=>{
                 >
                 
                 </FlatList>
+                <Button raised onPress={()=>{}  }>Sil</Button>
+            <Button raised onPress={()=>{}  }>Ekle</Button>
+
+
                 </View>
                     )
         }
@@ -84,6 +90,9 @@ fetchData=async ()=>{
             <View style={styles.container} >
             <Button raised onPress={()=>{this.setState({isShowListVisible:!this.state.isShowListVisible});}  }>Listele</Button>
 {this.renderFlatList()}
+
+
+
             </View>
             
         )
@@ -91,9 +100,8 @@ fetchData=async ()=>{
 }
 const styles=StyleSheet.create({
     container:{
-        flex:0.5,
-        justifyContent:"center",
-        alignItems:"center"
+        marginTop:20,
+       
     },
     list:{
     }
