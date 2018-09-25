3b916bc326e74b7b3677bead12dabbc235d1401f
Mac <gizemkorkmaz219@gmail.com>
2018-09-25 23:21:10 +0300

    bilgi

--- src/pages/FlatListExmPage.js
+++ src/pages/FlatListExmPage.js
@@ -120,3 +120,11 @@ const styles=StyleSheet.create({
     }
 })
 export {FlatListExmPage};
+
+// array.shift() // Dizinin ilk elemanını çıkartır.
+// array.unshift(0) // Diziye bir eleman ekler. (En başa)
+// // delete array[1] // Dizinin 1. anahtarlı elemanını çıkartır.
+// array.splice(2, 5) // Dizinin 2. elemanıyla 5. elemanı arasında kalan kısmını ayırır.
+// array.concat([8, 9, 10]) // Diziyi başka bir dizi ile birleştirir.
+// array.concat([11, 12, 13], [14, 15, 16]) // 3 diziyi birleştirir.
+// array.slice(0) // Dizinin verilen anahtarlı değerini diziden ayırır
